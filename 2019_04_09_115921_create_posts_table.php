c311fc1e4163abc64fe37362343f3f8ce5b2d546
hoangloc1509 <professorhoangloc@gmail.com>
2019-04-25 19:15:46 +0700

    docs: Add new init migration files.

---
+++ 2019_04_09_115921_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 255)->nullable(false);
+            $table->text('content');
+            $table->integer('views');
+            $table->integer('author_id')->default(0);
+            $table->string('image', 255)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
